7ebee817aa09a18827bc226817b75f2032b72009
blablablub100 <clemenstisch@hotmail.de>
2018-12-04 17:16:21 +0100

    Designed basic structure part 2

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,11 @@
 package java;
 
 public class Main {
+
+    public static void main(String[] args) {
+        //TODO: 1. CLI.getInput()
+        //TODO: 2. SimulationController.initSimulation()
+        //TODO: 3. SimulationController.getBoard()
+        //TODO: 4. CLI.printBoard()
+    }
 }
